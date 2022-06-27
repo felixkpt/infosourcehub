4f75c403056419ee6ba7f2639118b6c9a6498f8d
Felix Kiptoo <felixkpt@gmail.com>
2022-06-27 13:40:48 +0300

    Changed google G4 analytics code

--- wp-content/themes/b5awesometheme/header.php
+++ wp-content/themes/b5awesometheme/header.php
@@ -14,7 +14,7 @@
     <?php echo wp_head(); ?>
     <?php if ($_SERVER['HTTP_HOST'] != 'localhost' && !current_user_can('manage_options')) : ?>
         <!-- Global site tag (gtag.js) - Google Analytics -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-221500902-1"></script>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-D8SR0SRC3Z"></script>
         <script>
             window.dataLayer = window.dataLayer || [];
 
@@ -23,9 +23,8 @@
             }
             gtag('js', new Date());
 
-            gtag('config', 'UA-221500902-1');
+            gtag('config', 'G-D8SR0SRC3Z');
         </script>
-        <script data-ad-client="ca-pub-8286276782155723" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
     <?php endif; ?>
     <?php
     if (current_user_can('manage_options')) :
